9c102c4358e629015747a72c9968999ac98cda7e
Ngomi Patchamen Diane <patchbardi@yahoo.fr>
2024-10-30 14:55:43 +0000

    images

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,23 @@
 
 // src/Home.js
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; // Importez votre fichier CSS
 
 const Home = () => {
     return (
-        <div>
+        <div className="home-container"> {/* Appliquez la classe CSS */}
             <h1>Bienvenue sur mon profil utilisateur !</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Accueil</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 };
