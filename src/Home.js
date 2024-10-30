aba5541a4258d32c681589ee1bdbb5729c4a03ec
Ngomi Patchamen Diane <patchbardi@yahoo.fr>
2024-10-30 12:36:10 +0000

    Ajout du composant Home

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+
+// src/Home.js
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Bienvenue sur mon profil utilisateur !</h1>
+        </div>
+    );
+};
+
+export default Home;
