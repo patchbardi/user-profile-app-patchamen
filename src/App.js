953cbdcce2e5ac12c46b8de5c6c34c0000cd38c2
Ngomi Patchamen Diane <patchbardi@yahoo.fr>
2024-10-31 08:36:09 +0000

    Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './Home';
 import Contact from './Contact';
+import './index.css';
+import './App.css';
+
+
 
 const App = () => {
     return (
