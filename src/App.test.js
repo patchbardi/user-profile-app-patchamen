f5994f429c87e731ac465973b99f948ab3d19de1
Ngomi Patchamen Diane <patchbardi@yahoo.fr>
2024-11-07 11:16:37 +0000

    render,screen et FireEvent

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import FakeToggleLoginButton from './components/FakeToggleLoginButton';
+
+test('App should render', () => {
+    render(
+        <Router>
+            <App />
+        </Router>,
+    );
+    const text = screen.getByText(/Anmelden/i);
+    expect(text).toBeInTheDocument();
+});
+
+test('Fake login button should render', () => {
+    render(
+        <>
+            <FakeToggleLoginButton />
+        </>
+    );
+    let text = screen.getByText(/Einloggen/i);
+    expect(text).toBeInTheDocument();
+
+    render(
+        <>
+            <FakeToggleLoginButton isLoggedIn={true} />
+        </>
+    );
+    text = screen.getByText(/Ausloggen/i);
+    expect(text).toBeInTheDocument();
 });
